7d5e0b7c73519be9229b10792a879b489da5ad1d
Rafal Pietras <rafal.radx@gmail.com>
2024-06-22 13:10:33 +0200

    finished

---
+++ empiar_prep.py
@@ -0,0 +1,89 @@
+from dotenv import dotenv_values
+import os
+import shutil
+
+
+def split_files_into_batches(source_dir, batch_size=2000):
+    # Get the list of all files in the source directory
+    files = [
+        f for f in os.listdir(source_dir) if os.path.isfile(os.path.join(source_dir, f))
+    ]
+
+    # Calculate the number of batches required
+    num_batches = (len(files) + batch_size - 1) // batch_size
+
+    # subdir paths
+    subdir_paths = []
+
+    for n in range(num_batches):
+        # Create a new subdirectory for each batch
+        subdir_name = os.path.join(source_dir, f"subdir{n+1}")
+        os.makedirs(subdir_name, exist_ok=True)
+        subdir_paths.append(subdir_name)
+
+        # Get the files for the current batch
+        batch_files = files[n * batch_size : (n + 1) * batch_size]
+
+        for file_name in batch_files:
+            # Construct full file path
+            file_path = os.path.join(source_dir, file_name)
+            # Move file to the new subdirectory
+            shutil.move(file_path, subdir_name)
+
+    return subdir_paths
+
+
+if __name__ == "__main__":
+    # load configuration
+    config = dotenv_values("config.txt")
+
+    # splits files, returns paths to subdirs
+    subdirs = split_files_into_batches(
+        source_dir=config["TIFF_PATH"], batch_size=int(config["BATCH_SIZE"])
+    )
+
+    # current folder
+    current_path = os.getcwd()
+    # create dir for logs
+    log_dir = os.path.join(current_path, "transfer_logs")
+    os.makedirs(log_dir, exist_ok=True)
+
+    # prepare SLURM job script to transfer file from each subdir with aspera
+    NAME_ID = config["NAME_ID"]
+    for subdir in subdirs:
+        # folder name from the path
+        subdir_name = os.path.basename(subdir)
+
+        # lines of sbatch script to write
+        lines = [
+            "#!/bin/bash\n",
+            "#SBATCH --job-name=aspera_transfer\n",
+            f"#SBATCH --time={config['TIME']}\n",
+            f"#SBATCH --account={config['ACCOUNT']}\n",
+            "#SBATCH --partition=plgrid\n",
+            "#SBATCH --nodes=1\n",
+            "#SBATCH --ntasks-per-node=4\n",
+            "#SBATCH --cpus-per-task=1\n",
+            "#SBATCH --mem=10G\n",
+            f'#SBATCH --output="{log_dir}/{NAME_ID}_{subdir_name}_log.txt"\n',
+            f'#SBATCH --error="{log_dir}/{NAME_ID}_{subdir_name}_err.txt"\n',
+            "\n",
+        ]
+
+        # name of sbatch script for each batch of files
+        file_name = f"{NAME_ID}_{subdir_name}_empriar_upload.sh"
+
+        # ascp command
+        ascp_command_parts = [
+            f"ASPERA_SCP_PASS={config['ASPERA_SCP_PASS']}",
+            config["ASCP_PATH"],
+            f"-QT -P 33001 -l {config['TRANSFER_SPEED']} -L- -k3",
+            f"{config['TIFF_PATH']}/*.tiff",
+            f"emp_dep@hx-fasp-1.ebi.ac.uk:upload/{config['ASPERA_TOKEN']}/data",
+        ]
+
+        ascp_command = " ".join(ascp_command_parts)
+
+        with open(file_name, "w") as fn:
+            fn.writelines(lines)
+            fn.write(ascp_command)
